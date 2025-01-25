6337a87c1755f5c87e56138409feeea9299d9e16
Aliaksei Yafremau <447654972g@gmail.com>
2025-01-25 00:05:36 +0000

    Add cors

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import chat
 
 app = FastAPI(
     title="AI_PSY LangChain API",
     version="0.1.0",
     description="Психологический AI-ассистент, основанный на LangChain",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #TODO ИЗМЕНИТЬ НА РЕАЛЬНЫЙ ДОМЕН!!!
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(chat.router)
